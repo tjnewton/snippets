6a46d6448f52a296be5ac2de181de415693db43c
Tyler Newton <tjnewton.uni@gmail.com>
2021-11-01 09:29:57 -0700

    add dem2grid.py file

---
+++ grids/dem2grid.py
@@ -0,0 +1,6 @@
+# Script to sample a DEM on a grid of points using QGIS. This script should
+# be run in the QGIS Python Console because at the time of creation the
+# methods used below are not available in the QGIS API.
+
+
+
